7ef4be7d6de3be885cddd68b5ff089d84bbaa4f2
Asim <salimli.asim@gmail.com>
2019-03-20 10:37:25 +0000

    app renders list of items from e-bay depending on the search keyword

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -8,9 +8,10 @@ const SingleItem = ({item})=>{
             <div className="card">
               <img className="card-img-top" src={item.galleryURL} alt="Card image" /> 
               <div className="card-body">
+              <h5>${item.sellingStatus[0].currentPrice[0].__value__}</h5>
                 <h4 className="card-title"> {item.title[0].slice(0,25)}</h4>
                 <p className="card-text">Some example text.</p>
-                <a href={item.viewItemURL} className="btn btn-primary">See Profile</a>
+                <a href={item.viewItemURL} className="btn btn-primary">See Item</a>
               </div>
             </div>
 
